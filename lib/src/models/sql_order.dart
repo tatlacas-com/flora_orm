d77b3f4c10e8aca4d1810a3a2a6256617f17b126
Tatenda <tathove@gmail.com>
2021-12-12 11:13:58 +0200

    Update sql_order.dart

--- lib/src/models/sql_order.dart
+++ lib/src/models/sql_order.dart
@@ -1,6 +1,7 @@
+import 'package:equatable/equatable.dart';
 import 'package:tatlacas_sqflite_storage/sql.dart';
 
-class SqlOrder {
+class SqlOrder extends Equatable {
   final OrderDirection direction;
   final SqlColumn column;
 
@@ -8,6 +9,9 @@ class SqlOrder {
     this.direction = OrderDirection.Asc,
     required this.column,
   });
+
+  @override
+  List<Object?> get props => [direction, column];
 }
 
 enum OrderDirection {
