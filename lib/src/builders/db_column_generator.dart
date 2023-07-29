a366a59a85e1b088925a791ab2807abbecd053fc
Tatenda Caston <tathove@tatlacas.com>
2023-07-29 17:56:55 +0200

    properties with default values

--- lib/src/builders/db_column_generator.dart
+++ lib/src/builders/db_column_generator.dart
@@ -2,7 +2,6 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:tatlacas_sqflite_storage/src/builders/annotations.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:tatlacas_sqflite_storage/src/models/entity.dart';
 
 class DbColumnGenerator extends GeneratorForAnnotation<DbEntity> {
@@ -14,6 +13,8 @@ class DbColumnGenerator extends GeneratorForAnnotation<DbEntity> {
   ) {
     final classElement = element as ClassElement;
     final className = classElement.name;
+    final tableName = annotation.read('tableName').literalValue as String?;
+
     if (!TypeChecker.fromRuntime(Entity).isAssignableFrom(element)) {
       throw new Exception('$className is not an Entity class');
     }
@@ -29,7 +30,7 @@ class DbColumnGenerator extends GeneratorForAnnotation<DbEntity> {
 
     generatedCode.writeln('''
   @override
-  String get tableName => '${convertClassNameToSnakeCase(className)}';
+  String get tableName => '${tableName ?? convertClassNameToSnakeCase(className)}';
     ''');
     for (final field in fields) {
       if (TypeChecker.fromRuntime(DbColumn).hasAnnotationOfExact(field)) {
@@ -38,14 +39,88 @@ class DbColumnGenerator extends GeneratorForAnnotation<DbEntity> {
         final fieldType = field.type.getDisplayString(withNullability: false);
 
         columnsList.writeln('column$fieldNameCamel,');
-        generatedCode.writeln('''
+        final metadata = field.metadata;
+        for (final annotationElement in metadata) {
+          final element = annotationElement.element;
+          if (element != null &&
+              TypeChecker.fromRuntime(DbColumn).isExactly(element)) {
+            // Get the constructor arguments of the DbColumn annotation
+            final constantObj = annotationElement.computeConstantValue();
+
+            final String name =
+                constantObj?.getField('name')?.toStringValue() ?? fieldName;
+            final String? alias =
+                constantObj?.getField('alias')?.toStringValue();
+            final bool jsonEncodeAlias =
+                constantObj?.getField('jsonEncodeAlias')?.toBoolValue() ??
+                    false;
+            final bool primaryKey =
+                constantObj?.getField('primaryKey')?.toBoolValue() ?? false;
+            final bool autoIncrementPrimary =
+                constantObj?.getField('autoIncrementPrimary')?.toBoolValue() ??
+                    false;
+            final bool notNull =
+                constantObj?.getField('notNull')?.toBoolValue() ?? false;
+            final bool unique =
+                constantObj?.getField('unique')?.toBoolValue() ?? false;
+            final dynamic defaultValue =
+                constantObj?.getField('defaultValue')?.toStringValue();
+
+            generatedCode.writeln('''
       SqlColumn<$className, $fieldType> get column$fieldNameCamel =>
         SqlColumn<$className, $fieldType>(
-          '$fieldName',
+          '$name',
+    ''');
+            if (alias != null) {
+              generatedCode.writeln('''
+           alias: '$alias',
+    ''');
+            }
+            if (jsonEncodeAlias == true) {
+              generatedCode.writeln('''
+           jsonEncodeAlias: $jsonEncodeAlias,
+    ''');
+            }
+            if (primaryKey == true) {
+              generatedCode.writeln('''
+           primaryKey: $primaryKey,
+    ''');
+            }
+            if (unique == true) {
+              generatedCode.writeln('''
+           unique: $unique,
+    ''');
+            }
+            if (autoIncrementPrimary == true) {
+              generatedCode.writeln('''
+           autoIncrementPrimary: $autoIncrementPrimary,
+    ''');
+            }
+            if (notNull == true) {
+              generatedCode.writeln('''
+           notNull: $notNull,
+    ''');
+            }
+            if (defaultValue != null) {
+              if (field.type.isDartCoreString) {
+                generatedCode.writeln('''
+           defaultValue: '$defaultValue',
+    ''');
+              } else {
+                generatedCode.writeln('''
+           defaultValue: $defaultValue,
+    ''');
+              }
+            }
+            generatedCode.writeln('''
           saveToDb: (entity) => entity.$fieldName,
           readFromDb: (entity, value) => entity.copyWith($fieldName: value),
         );
     ''');
+
+            break;
+          }
+        }
       }
     }
     generatedCode.writeln('''
