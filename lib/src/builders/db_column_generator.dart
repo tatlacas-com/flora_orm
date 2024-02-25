82f76a3b7b60964ea01e77e62a98abaf58918abf
Tatenda Caston <tathove@tatlacas.com>
2024-02-25 19:27:19 +0200

    updates

--- lib/src/builders/db_column_generator.dart
+++ lib/src/builders/db_column_generator.dart
@@ -64,129 +64,127 @@ class DbColumnGenerator extends GeneratorForAnnotation<DbEntity> {
   String get tableName => '${tableName ?? convertClassNameToSnakeCase(className)}';
     ''');
     for (final field in fields) {
-      if (const TypeChecker.fromRuntime(DbColumn).hasAnnotationOfExact(field)) {
-        final fieldName = field.name;
-        final fieldNameCamel = _toUpperCamelCase(fieldName);
-        final fieldType = field.type.getDisplayString(withNullability: false);
-        final fieldTypeFull =
-            field.type.getDisplayString(withNullability: true);
+      // if (const TypeChecker.fromRuntime(DbColumn).hasAnnotationOfExact(field)) {
+      final fieldName = field.name;
+      final fieldNameCamel = _toUpperCamelCase(fieldName);
+      final fieldType = field.type.getDisplayString(withNullability: false);
+      final fieldTypeFull = field.type.getDisplayString(withNullability: true);
 
-        columnsList.writeln('column$fieldNameCamel,');
-        propsList.writeln('$fieldName,');
-        getList.writeln('$fieldTypeFull get $fieldName;');
-        final fieldMetadata = field.metadata;
-        if (fieldMetadata.isEmpty) {
+      columnsList.writeln('column$fieldNameCamel,');
+      propsList.writeln('$fieldName,');
+      getList.writeln('$fieldTypeFull get $fieldName;');
+      final fieldMetadata = field.metadata;
+      if (fieldMetadata.isEmpty) {
+        extraFields[fieldName] = _ExtraField(
+          type: fieldType,
+          notNull: field.type.nullabilitySuffix == NullabilitySuffix.none,
+        );
+        continue;
+      }
+      final List<ElementAnnotation> fieldAnnotations = [];
+      final List<ElementAnnotation> nullableProps = [];
+      for (final annotation in fieldMetadata) {
+        final tp = annotation.computeConstantValue()?.type;
+        if (tp == null) {
           extraFields[fieldName] = _ExtraField(
             type: fieldType,
             notNull: field.type.nullabilitySuffix == NullabilitySuffix.none,
           );
           continue;
         }
-        final List<ElementAnnotation> fieldAnnotations = [];
-        final List<ElementAnnotation> nullableProps = [];
-        for (final annotation in fieldMetadata) {
-          final tp = annotation.computeConstantValue()?.type;
-          if (tp == null) {
-            extraFields[fieldName] = _ExtraField(
-              type: fieldType,
-              notNull: field.type.nullabilitySuffix == NullabilitySuffix.none,
-            );
-            continue;
-          }
-          if (const TypeChecker.fromRuntime(DbColumn).isExactlyType(tp)) {
-            fieldAnnotations.add(annotation);
-            continue;
-          }
-          if (const TypeChecker.fromRuntime(NullableProp).isExactlyType(tp)) {
-            nullableProps.add(annotation);
-            continue;
-          }
-          extraFields[fieldName] = _ExtraField(
-            type: fieldType,
-            notNull: field.type.nullabilitySuffix == NullabilitySuffix.none,
-          );
+        if (const TypeChecker.fromRuntime(DbColumn).isExactlyType(tp)) {
+          fieldAnnotations.add(annotation);
+          continue;
         }
+        if (const TypeChecker.fromRuntime(NullableProp).isExactlyType(tp)) {
+          nullableProps.add(annotation);
+          continue;
+        }
+        extraFields[fieldName] = _ExtraField(
+          type: fieldType,
+          notNull: field.type.nullabilitySuffix == NullabilitySuffix.none,
+        );
+      }
 
-        for (final annotation in fieldAnnotations) {
-          final dbColumnAnnotation = annotation.computeConstantValue()!;
+      for (final annotation in fieldAnnotations) {
+        final dbColumnAnnotation = annotation.computeConstantValue()!;
 
-          final String name =
-              dbColumnAnnotation.getField('name')?.toStringValue() ?? fieldName;
-          final jsonEncoded =
-              dbColumnAnnotation.getField('encodedJson')?.toBoolValue() ??
-                  false;
-          final String? alias =
-              dbColumnAnnotation.getField('alias')?.toStringValue() ??
-                  (jsonEncoded ? fieldName.replaceAll('Json', '') : null);
+        final String name =
+            dbColumnAnnotation.getField('name')?.toStringValue() ?? fieldName;
+        final jsonEncoded =
+            dbColumnAnnotation.getField('encodedJson')?.toBoolValue() ?? false;
+        final String? alias =
+            dbColumnAnnotation.getField('alias')?.toStringValue() ??
+                (jsonEncoded ? fieldName.replaceAll('Json', '') : null);
 
-          final String? writeFn =
-              dbColumnAnnotation.getField('writeFn')?.toStringValue();
+        final String? writeFn =
+            dbColumnAnnotation.getField('writeFn')?.toStringValue();
 
-          final bool hasRead =
-              dbColumnAnnotation.getField('hasRead')?.toBoolValue() ?? false;
-          final bool hasWrite =
-              dbColumnAnnotation.getField('hasWrite')?.toBoolValue() ?? false;
-          final bool primaryKey =
-              dbColumnAnnotation.getField('primaryKey')?.toBoolValue() ?? false;
-          final bool autoIncrementPrimary = dbColumnAnnotation
-                  .getField('autoIncrementPrimary')
-                  ?.toBoolValue() ??
-              false;
-          final bool notNull =
-              dbColumnAnnotation.getField('notNull')?.toBoolValue() ??
-                  (field.type.nullabilitySuffix == NullabilitySuffix.none);
-          final bool unique =
-              dbColumnAnnotation.getField('unique')?.toBoolValue() ?? false;
-          dynamic defaultValue = dbColumnAnnotation.getField('defaultValue');
-          if (field.type.isDartCoreBool) {
-            defaultValue =
-                dbColumnAnnotation.getField('defaultValue')?.toBoolValue();
-          } else if (field.type.isDartCoreInt) {
-            defaultValue =
-                dbColumnAnnotation.getField('defaultValue')?.toIntValue();
-          } else if (field.type.isDartCoreDouble) {
-            defaultValue =
-                dbColumnAnnotation.getField('defaultValue')?.toDoubleValue();
+        final bool hasRead =
+            dbColumnAnnotation.getField('hasRead')?.toBoolValue() ?? false;
+        final bool hasWrite =
+            dbColumnAnnotation.getField('hasWrite')?.toBoolValue() ?? false;
+        final bool primaryKey =
+            dbColumnAnnotation.getField('primaryKey')?.toBoolValue() ?? false;
+        final bool autoIncrementPrimary = dbColumnAnnotation
+                .getField('autoIncrementPrimary')
+                ?.toBoolValue() ??
+            false;
+        final bool notNull =
+            dbColumnAnnotation.getField('notNull')?.toBoolValue() ??
+                (field.type.nullabilitySuffix == NullabilitySuffix.none);
+        final bool unique =
+            dbColumnAnnotation.getField('unique')?.toBoolValue() ?? false;
+        dynamic defaultValue = dbColumnAnnotation.getField('defaultValue');
+        if (field.type.isDartCoreBool) {
+          defaultValue =
+              dbColumnAnnotation.getField('defaultValue')?.toBoolValue();
+        } else if (field.type.isDartCoreInt) {
+          defaultValue =
+              dbColumnAnnotation.getField('defaultValue')?.toIntValue();
+        } else if (field.type.isDartCoreDouble) {
+          defaultValue =
+              dbColumnAnnotation.getField('defaultValue')?.toDoubleValue();
+        } else {
+          defaultValue =
+              dbColumnAnnotation.getField('defaultValue')?.toStringValue();
+        }
+        var columnType = fieldType;
+        String? jsonEncodedType;
+        final annotationSource = annotation.toSource().trim();
+
+        final start = annotationSource.indexOf('<');
+        final end = annotationSource.indexOf('>');
+        if (start != -1) {
+          final t = annotationSource.substring(start + 1, end);
+          if (jsonEncoded) {
+            jsonEncodedType = t;
           } else {
-            defaultValue =
-                dbColumnAnnotation.getField('defaultValue')?.toStringValue();
+            columnType = t;
           }
-          var columnType = fieldType;
-          String? jsonEncodedType;
-          final annotationSource = annotation.toSource().trim();
-
-          final start = annotationSource.indexOf('<');
-          final end = annotationSource.indexOf('>');
-          if (start != -1) {
-            final t = annotationSource.substring(start + 1, end);
-            if (jsonEncoded) {
-              jsonEncodedType = t;
-            } else {
-              columnType = t;
-            }
+        }
+        FieldElement? aliasProperty;
+        bool aliasNotNull = false;
+        if (alias != null && (hasRead || jsonEncoded)) {
+          final finder = PropertyFinder(alias);
+          classElement.accept(finder);
+          aliasProperty = finder.foundProperty!;
+          aliasNotNull =
+              aliasProperty.type.nullabilitySuffix == NullabilitySuffix.none;
+          if (aliasProperty.type.isDartCoreList) {
+            jsonEncodedType =
+                aliasProperty.type.getDisplayString(withNullability: false);
           }
-          FieldElement? aliasProperty;
-          bool aliasNotNull = false;
-          if (alias != null && (hasRead || jsonEncoded)) {
-            final finder = PropertyFinder(alias);
-            classElement.accept(finder);
-            aliasProperty = finder.foundProperty!;
-            aliasNotNull =
-                aliasProperty.type.nullabilitySuffix == NullabilitySuffix.none;
-            if (aliasProperty.type.isDartCoreList) {
-              jsonEncodedType =
-                  aliasProperty.type.getDisplayString(withNullability: false);
-            }
-            generatedCode.writeln('''
+          generatedCode.writeln('''
   $jsonEncodedType? get $alias;
  ''');
-          }
-          if (hasRead) {
-            generatedCode.writeln('''
+        }
+        if (hasRead) {
+          generatedCode.writeln('''
   $className read$fieldNameCamel(Map<String, dynamic> json, value, $className entity);
  ''');
-          } else if (jsonEncoded) {
-            generatedCode.writeln('''
+        } else if (jsonEncoded) {
+          generatedCode.writeln('''
   $className read$fieldNameCamel(Map<String, dynamic> json, value, $className entity){
     $jsonEncodedType? $alias;
     final val = value != null && value != 'null' ? value : null;
@@ -201,107 +199,107 @@ class DbColumnGenerator extends GeneratorForAnnotation<DbEntity> {
     );
   }
  ''');
-          }
-          if (hasWrite) {
-            generatedCode.writeln('''
+        }
+        if (hasWrite) {
+          generatedCode.writeln('''
   $columnType? write$fieldNameCamel($className entity);
  ''');
-          }
+        }
 
-          generatedCode.writeln('''
+        generatedCode.writeln('''
       SqlColumn<$className, $columnType> get column$fieldNameCamel =>
         SqlColumn<$className, $columnType>(
           '$name',
     ''');
-          if (alias != null) {
-            generatedCode.writeln('''
+        if (alias != null) {
+          generatedCode.writeln('''
            alias: '$alias',
     ''');
-          }
-          if (jsonEncoded) {
-            generatedCode.writeln('''
+        }
+        if (jsonEncoded) {
+          generatedCode.writeln('''
            jsonEncodeAlias: $jsonEncoded,
     ''');
-          }
-          if (primaryKey == true) {
-            generatedCode.writeln('''
+        }
+        if (primaryKey == true) {
+          generatedCode.writeln('''
            primaryKey: $primaryKey,
     ''');
-          }
-          if (unique == true) {
-            generatedCode.writeln('''
+        }
+        if (unique == true) {
+          generatedCode.writeln('''
            unique: $unique,
     ''');
-          }
-          if (autoIncrementPrimary == true) {
-            generatedCode.writeln('''
+        }
+        if (autoIncrementPrimary == true) {
+          generatedCode.writeln('''
            autoIncrementPrimary: $autoIncrementPrimary,
     ''');
-          }
-          if (notNull == true) {
-            generatedCode.writeln('''
+        }
+        if (notNull == true) {
+          generatedCode.writeln('''
            notNull: $notNull,
     ''');
-          }
-          if (defaultValue != null) {
-            if (field.type.isDartCoreString) {
-              generatedCode.writeln('''
+        }
+        if (defaultValue != null) {
+          if (field.type.isDartCoreString) {
+            generatedCode.writeln('''
            defaultValue: '$defaultValue',
-    ''');
-            } else {
-              generatedCode.writeln('''
-           defaultValue: $defaultValue,
-    ''');
-            }
-          }
-          if (hasWrite) {
-            generatedCode.writeln('''
-          write: (entity) => write$fieldNameCamel(entity),
-    ''');
-          } else if (jsonEncoded) {
-            generatedCode.writeln('''
-          write: (entity) {
-    ''');
-            final finder = PropertyFinder(alias!);
-            classElement.accept(finder);
-            final property = finder.foundProperty!;
-            if (property.type.isDartCoreList) {
-              jsonEncodedType =
-                  property.type.getDisplayString(withNullability: false);
-              generatedCode.writeln('''
-            final map = entity.$alias?.map((p) => p.toMap()).toList();
-    ''');
-            } else {
-              generatedCode.writeln('''
-            final map = entity.$alias?.${writeFn ?? 'toMap'}();
-    ''');
-            }
-            generatedCode.writeln('''
-            return jsonEncode(map);
-            },
     ''');
           } else {
             generatedCode.writeln('''
-          write: (entity) => entity.$fieldName,
+           defaultValue: $defaultValue,
     ''');
           }
-          if (hasRead || jsonEncoded) {
-            if (jsonEncoded) {
-              if (alias != null) {
-                if (aliasNotNull) {
-                  copyWithPropsList.writeln('$jsonEncodedType? $alias,');
-                  copyWithList.writeln('$alias: $alias ?? this.$alias,');
-                } else {
-                  copyWithPropsList
-                      .writeln('CopyWith<$jsonEncodedType?>? $alias,');
-                  copyWithList.writeln(
-                      '$alias: $alias != null ? $alias.value : this.$alias,');
-                }
-                if (extraFields.containsKey(alias)) {
-                  extraFields.remove(alias);
-                }
+        }
+        if (hasWrite) {
+          generatedCode.writeln('''
+          write: (entity) => write$fieldNameCamel(entity),
+    ''');
+        } else if (jsonEncoded) {
+          generatedCode.writeln('''
+          write: (entity) {
+    ''');
+          final finder = PropertyFinder(alias!);
+          classElement.accept(finder);
+          final property = finder.foundProperty!;
+          if (property.type.isDartCoreList) {
+            jsonEncodedType =
+                property.type.getDisplayString(withNullability: false);
+            generatedCode.writeln('''
+            final map = entity.$alias?.map((p) => p.toMap()).toList();
+    ''');
+          } else {
+            generatedCode.writeln('''
+            final map = entity.$alias?.${writeFn ?? 'toMap'}();
+    ''');
+          }
+          generatedCode.writeln('''
+            return jsonEncode(map);
+            },
+    ''');
+        } else {
+          generatedCode.writeln('''
+          write: (entity) => entity.$fieldName,
+    ''');
+        }
+        if (hasRead || jsonEncoded) {
+          if (jsonEncoded) {
+            if (alias != null) {
+              if (aliasNotNull) {
+                copyWithPropsList.writeln('$jsonEncodedType? $alias,');
+                copyWithList.writeln('$alias: $alias ?? this.$alias,');
+              } else {
+                copyWithPropsList
+                    .writeln('CopyWith<$jsonEncodedType?>? $alias,');
+                copyWithList.writeln(
+                    '$alias: $alias != null ? $alias.value : this.$alias,');
               }
-              generatedCode.writeln('''
+              if (extraFields.containsKey(alias)) {
+                extraFields.remove(alias);
+              }
+            }
+            generatedCode.writeln('''
           read: (json, entity, value){
             if ('null' == value){
               return read$fieldNameCamel(json, null, entity);
@@ -310,42 +308,42 @@ class DbColumnGenerator extends GeneratorForAnnotation<DbEntity> {
           },
         );
     ''');
-            } else {
-              generatedCode.writeln('''
+          } else {
+            generatedCode.writeln('''
           read: (json, entity, value) => read$fieldNameCamel(json, value, entity),
         );
     ''');
-            }
-          } else if (notNull) {
-            generatedCode.writeln('''
+          }
+        } else if (notNull) {
+          generatedCode.writeln('''
           read: (json, entity, value) => entity.copyWith($fieldName: value, json: json),
         );
     ''');
-          } else {
-            generatedCode.writeln('''
+        } else {
+          generatedCode.writeln('''
           read: (json, entity, value) => entity.copyWith($fieldName: CopyWith(value), json: json),
         );
     ''');
-          }
-          if (notNull) {
-            copyWithPropsList.writeln('$fieldType? $fieldName,');
-            copyWithList.writeln('$fieldName: $fieldName ?? this.$fieldName,');
-          } else {
-            copyWithPropsList.writeln('CopyWith<$fieldType?>? $fieldName,');
-            copyWithList.writeln(
-                '$fieldName: $fieldName != null ? $fieldName.value : this.$fieldName,');
-          }
-
-          if (extraFields.containsKey(fieldName)) {
-            extraFields.remove(fieldName);
-          }
         }
-        for (final _ in nullableProps) {
+        if (notNull) {
+          copyWithPropsList.writeln('$fieldType? $fieldName,');
+          copyWithList.writeln('$fieldName: $fieldName ?? this.$fieldName,');
+        } else {
           copyWithPropsList.writeln('CopyWith<$fieldType?>? $fieldName,');
           copyWithList.writeln(
               '$fieldName: $fieldName != null ? $fieldName.value : this.$fieldName,');
         }
+
+        if (extraFields.containsKey(fieldName)) {
+          extraFields.remove(fieldName);
+        }
       }
+      for (final _ in nullableProps) {
+        copyWithPropsList.writeln('CopyWith<$fieldType?>? $fieldName,');
+        copyWithList.writeln(
+            '$fieldName: $fieldName != null ? $fieldName.value : this.$fieldName,');
+      }
+      // }
     }
     for (final fieldName in extraFields.keys) {
       final extraField = extraFields[fieldName]!;
